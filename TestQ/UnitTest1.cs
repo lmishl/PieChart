74bda392f7598282d7e9fe946d775a0cdedd4a55
Laptev Mikhail <bugi-wugi93@mail.ru>
2015-04-28 20:34:08 +0700

    add Test Project

---
+++ TestQ/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestQ
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            //BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act
+           // account.Debit(debitAmount);
+
+            // assert
+            double actual = 56;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+    }
+}
